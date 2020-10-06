9b8cd4392f4cc46116ff197ba2aaed023d6625d5
Tirth Chaudhary <tirthchaudhary9960@gmail.com>
2020-10-06 12:24:16 +0530

    user profiling 1.0

---
+++ src/auth/components/DataList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+const DataList = (props) => {
+    const { data } = props;
+    const CellFormatter = (cell, row) => {
+        return (<div><a target="_blank" rel="noopener noreferrer" href={"https://codeforces.com/contest/" + row.id}>{cell}</a></div>);
+    };
+    const columns = [
+        { dataField: 'id', text: 'Id', sort: true },
+        { dataField: 'problem.name', text: 'Name', sort: true },
+        { dataField: 'problem.index', text: 'Level', sort: true },
+        { dataField: 'author.participantType', text: 'Type', sort: true },
+        { dataField: 'programmingLanguage', text: 'Lang', sort: true },
+        { dataField: 'verdict', text: 'Verdict', sort: true },
+    ];
+
+    if (data !== null) {
+        if (data.status !== "OK")
+            return <p>Some Problem in loading data...</p>
+        else {
+            return <BootstrapTable wrapperClasses="mytable" hover striped keyField='id' data={data.result} columns={columns} pagination={paginationFactory()} />;
+        }
+    }
+    return (
+        <p>Wait...</p>
+    );
+
+}
+export default DataList;
--- src/auth/components/Profile.js
+++ src/auth/components/Profile.js
@@ -1,30 +1,79 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import AuthService from "../services/auth.service";
+import ListLoading from "../../Components/ListLoading";
+import DataList from "./DataList";
+
 
 const Profile = () => {
-    const currentUser = AuthService.getUser();
+    const ListLoader = ListLoading();
+    const [userHandle, setUserHandle] = useState("tirth9960");
+    const [loading, setLoading] = useState(false);
+    const [currentUser, setCurrentUser] = useState({});
+    const [userData, setUserData] = useState({});
+    const cache = useRef({});
+    const cuser = AuthService.getUser();
+    if (cuser) {
+        setCurrentUser(cuser);
+    }
+    useEffect(() => {
+        if (currentUser && currentUser.handle) {
+            setUserHandle(currentUser.handle);
+        }
+        const fetchData = async () => {
+            const apiUrl = `https://codeforces.com/api/user.status?handle=${userHandle}`;
+            let res = {};
+            setLoading(true);
+            if (cache.current[userHandle]) {
+                res = cache.current[userHandle];
+                setLoading(false);
+            }
+            else {
+                const response = await fetch(apiUrl);
+                res = await response.json();
+                cache.current[userHandle] = res;
+                setLoading(false);
+            }
+            setUserData(res);
+        }
+        fetchData();
+    }, [currentUser]);
 
-    return (
-        <div className="container">
-            <header className="jumbotron">
-                <h3>
-                    <strong>{currentUser.username}</strong> Profile
-                </h3>
-            </header>
+    if (currentUser && currentUser.username) {
+        return (
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>
+                        <strong>{currentUser.username}</strong> Profile
+                    </h3>
+                </header>
 
-            <p>
-                <strong>Id:</strong>{currentUser.id}
-            </p>
-            <p>
-                <strong>Email:</strong>{currentUser.email}
-            </p>
-            <strong>Authorities:</strong>
-            <ul>
-                {currentUser.roles &&
-                    currentUser.roles.map((role, index) => <li key={index}>{role}</li>)
-                }
-            </ul>
-        </div>
-    );
+                <p>
+                    <strong>Id:</strong>{currentUser.id}
+                </p>
+                <p>
+                    <strong>Email:</strong>{currentUser.email}
+                </p>
+                <strong>Authorities:</strong>
+                <ul>
+                    {currentUser.roles &&
+                        currentUser.roles.map((role, index) => <li key={index}>{role}</li>)
+                    }
+                </ul>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className="container">
+                <header className="alert alert-danger mt-5">
+                    <h3>
+                        Unauthorized Access!!
+                    </h3>
+                </header>
+                <ListLoader List={DataList} data={userData} isLoading={loading} />
+
+            </div>
+        );
+    }
 };
 export default Profile;
